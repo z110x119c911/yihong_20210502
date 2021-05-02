552db812089e9fe2c04b569724958ace2472690c
james HE <james840525@gmail.com>
2021-05-02 18:10:05 +0800

    out html

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/yihong_20210502/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './dist/' : './',
+  indexPath: process.env.NODE_ENV === 'production' ? '../index.html/' : '/',
 }
